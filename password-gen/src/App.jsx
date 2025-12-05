72444d5a884578aa437e927b1c938490eb56dd29
Sivakumar <sivakumarr3105@gmail.com>
2025-12-05 23:45:41 +0530

    setup done

---
+++ password-gen/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Generator from "./components/Generator"
+function App() {
+
+  return (
+    <>
+    <Generator/>
+    </>
+  )
+}
+
+export default App
---
+++ password-gen/src/components/Generator.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Generator = () => {
+  return (
+    <div className='flex flex-col pt-10 justify-center  items-center'>
+        <div className='text-center justify-center items-center border rounded-xl border-black h-90 w-130'>  
+            <h3 className='text-xl font-boldS'>PASSWORD GENERATOR</h3>
+            <div className='border py-5 w-90  gap-2 justify-center mt-5 ml-20'>
+                <p></p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Generator
